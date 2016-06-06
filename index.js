fd86c471f68f17edda40995625591b9251ea697a
Rob Loach <robloach@gmail.com>
2016-06-06 09:39:55 -0400

    Update to sweet.js@^1.0.3

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 var sweet = require('sweet.js');
+var extend = require('extend-shallow');
 
 exports.name = 'sweetjs';
-exports.inputFormats = ['sweetjs', 'sweetjsbar'];
+exports.inputFormats = ['sweetjs', 'sweet'];
 exports.outputFormat = 'html';
 
-exports.render = function (str, options) {
-  var output = sweet.compile(str, options);
+exports.render = function (str, options, locals) {
+  var opts = extend({}, options, locals);
+  var output = sweet.compile(str, opts);
   return output.code;
-}
+};
