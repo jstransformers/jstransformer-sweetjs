f59974ea7a8ab4c6b50d7f89eda615742e34cf53
Rob Loach <robloach@gmail.com>
2018-02-15 10:43:40 -0500

    Update Dependencies

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 'use strict'
 
 const extend = require('extend-shallow')
-const sweet = require('sweet.js')
+const sweet = require('@sweet-js/core')
+const NodeLoader = require('@sweet-js/core/dist/node-loader').default
 
 exports.name = 'sweetjs'
-exports.inputFormats = ['sweetjs', 'sweet']
+exports.inputFormats = ['sweetjs', 'sweet', 'sjs']
 exports.outputFormat = 'html'
 
-exports.render = function (str, options, locals) {
-  const opts = extend({}, options, locals)
-  const output = sweet.compile(str, opts)
-  return output.code
+exports.renderFile = function (file, options, locals) {
+  extend(options, locals)
+  const loader = new NodeLoader(process.cwd(), options)
+  const out = sweet.compile(file, loader, options)
+  return out.code
 }
